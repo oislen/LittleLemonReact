0a5e98b1461983c89f38e7094325dc480c616964
Oisin <oisin.leonard@gmail.com>
2025-10-04 19:21:05 +0100

    Added navbar and logo

--- littlelemon/src/components/Nav.js
+++ littlelemon/src/components/Nav.js
@@ -1,6 +1,24 @@
+import logo from "../assets/Logo.svg";
+
 const Nav = () => {
   return (
-    <nav className="nav"></nav>
+    <nav className="nav">
+        <img src={logo} alt="logo" />
+      <ul className="nav-links">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Menu</a>
+        </li>
+        <li>
+          <a href="/">Reservations</a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
