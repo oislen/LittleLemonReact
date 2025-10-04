8b81def6c15a3c091dd3bd92828541669f82cbec
Oisin <oisin.leonard@gmail.com>
2025-10-04 21:51:32 +0100

    Added routing to other pages in main

--- littlelemon/src/components/Main.js
+++ littlelemon/src/components/Main.js
@@ -1,6 +1,21 @@
+import { BrowserRouter as Router, Routes, Route   } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About"
+import Menu from "./pages/Menu"
+import Bookings from "./pages/Bookings"
+
 const Main = () => {
   return (
-    <main className="main"></main>
+    <main>
+      <Router>
+        <Routes>
+          <Route  path="/" element={<Home />} />
+          <Route  path="/about" element={<About />} />
+          <Route  path="/menu" element={<Menu />} />
+          <Route  path="/bookings" element={<Bookings />} />
+        </Routes>
+      </Router>
+    </main>
   );
 };
 
