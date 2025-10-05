e1a858fa0d344df3a041781e21049d3ef5cb5a11
Oisin <oisin.leonard@gmail.com>
2025-10-05 13:21:57 +0100

    Extracted navbar from header component

--- littlelemon/src/components/Header.js
+++ littlelemon/src/components/Header.js
@@ -1,25 +1,9 @@
-import logo from "../assets/Logo.svg";
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
-    <header className="header">
-      <nav className="navbar">
-          <img src={logo} alt="logo" />
-        <ul className="navbar-links">
-          <li>
-            <a className="navbar-button" href="/">Home</a>
-          </li>
-          <li>
-            <a className="navbar-button" href="/about">About</a>
-          </li>
-          <li>
-            <a className="navbar-button" href="/menu">Menu</a>
-          </li>
-          <li>
-            <a className="navbar-button" href="/bookings">Bookings</a>
-          </li>
-        </ul>
-      </nav>
+    <header>
+      <img src={logo} alt="logo" />
     </header>
   );
 };
