f67d85b2c1a3c2c385f0c4f7f271b1fb943e7d7a
Oisin <oisin.leonard@gmail.com>
2025-10-05 13:24:10 +0100

    Updated home page using template from html and css module

--- littlelemon/src/components/pages/Home.js
+++ littlelemon/src/components/pages/Home.js
@@ -1,22 +1,54 @@
-import bannerImg from "../../assets/restauranfood.jpg";
+import Hero from "./partials/Hero"
+import menuImg from "../../assets/Grilled fish C.jpg";
+import bookImg from "../../assets/salad.jpg";
+import openImg from "../../assets/Mario and Adrian b.jpg";
 
 const Home = () => {
   return (
-    <div className="webPage">
+    <>
       <section>
-        <div className="banner">
-          <h2>Little Lemon</h2>
-          <h3>Chicago</h3>
+        <article id="hero">
+          <h1>30% Off This Weekend</h1>
           <p>
-            We are a family owned Mediterraneran restaurant, focused on
-            traditional recipes served with a modern twist.
+              Enjoy an exclusive special deal on your bill this weekend only. Treat yourself to your favorites and make the most of this limited-time offer before it's gone.
           </p>
-        </div>
-        <div className="banner-img">
-          <img src={bannerImg} alt="" />
-        </div>
+        </article>
       </section>
-    </div>
+      <section>
+        <article>
+          <h2>Our New Menu</h2>
+          <img src={menuImg} />
+          <p>
+            Experience the vibrant flavors of our new Mediterranean menu, inspired by fresh ingredients and timeless coastal recipes. From savory mezze to fragrant grilled specialties, every dish is crafted to transport you to the sun-kissed shores of the Mediterranean.
+          </p>
+          <p>
+            <a href="/menu">See our new menu</a>
+          </p>
+        </article>
+        <article>
+          <h2>Book a table</h2>
+          <img src={bookImg} />
+          <p>
+            Reserve your table today and make sure you don't miss out on an unforgettable dining experience. Whether it's a romantic evening or a gathering with friends, we'll have everything ready to welcome you.
+          </p>
+          <p>
+            <a href="/book">Book your table now</a>
+          </p>
+        </article>
+        <article>
+          <h2>Opening Hours</h2>
+          <img src={openImg} />
+          <p>
+            The Little Lemon Restaurant is open 7 days a week, except for public holidays. 
+          </p>
+          <ul>
+            <li>Mon - Fri: 2pm - 10pm</li>
+            <li>Sat: 2pm - 11pm</li>
+            <li>Sun: 2pm - 9pm</li>
+          </ul>
+        </article>
+      </section>
+    </>
   );
 };
 
