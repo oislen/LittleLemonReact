82d42655238f50a902d379de404ee75e8f51524d
Oisin <oisin.leonard@gmail.com>
2025-10-05 13:19:52 +0100

    Moved navigation bar to separate component

---
+++ littlelemon/src/components/Nav.js
@@ -0,0 +1,22 @@
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/menu">Menu</a>
+        </li>
+        <li>
+          <a href="/book">Book</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
