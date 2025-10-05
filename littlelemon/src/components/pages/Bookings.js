a81409026e6e8859b0ce038354022c051dc57f48
Oisin <oisin.leonard@gmail.com>
2025-10-05 15:13:56 +0100

    Added more images to assets. Swapped map on book with image on about

--- littlelemon/src/components/pages/Bookings.js
+++ littlelemon/src/components/pages/Bookings.js
@@ -1,11 +1,26 @@
 import BookingForm from "./forms/BookingForm";
+import aboutImage from "../../assets/lemon dessert B.jpg";
 
 const Bookings = () => {
   return (
     <>
       <section>
-        <h2>Bookings</h2>
-        <BookingForm />
+        <article>
+          <div className="row">
+            <div className="column">
+              <h2>Bookings</h2>
+              <BookingForm />
+            </div>
+            <div className="column">
+              <figure className="figure" >
+                <img src={aboutImage}  alt="Mario and Adrian"/>
+                <figcaption className="figure-caption">
+                  Little Lemon owners Mario and Adrian.
+                </figcaption>
+              </figure>
+            </div>
+          </div>
+        </article>
       </section>
     </>
   );
