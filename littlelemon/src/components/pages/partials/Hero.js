8853c25606b891eb912a4f9f2419971b0f049489
Oisin <oisin.leonard@gmail.com>
2025-10-05 13:20:17 +0100

    Created hero component

---
+++ littlelemon/src/components/pages/partials/Hero.js
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <section>
+      <article id="hero">
+        <h1>SPECIAL OFFER</h1>
+          <p>30% Off This Weekend</p>
+      </article>
+    </section>
+  );
+};
+
+export default Hero;
