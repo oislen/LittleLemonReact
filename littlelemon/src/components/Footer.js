bf026a1edbec30e25e6e7f743f003750e3306dca
Oisin <oisin.leonard@gmail.com>
2025-10-05 13:21:35 +0100

    Created footer component

--- littlelemon/src/components/Footer.js
+++ littlelemon/src/components/Footer.js
@@ -1,6 +1,15 @@
+import footerImage from "../assets/logo_footer.png";
+
 const Footer = () => {
   return (
-    <footer className="footer"></footer>
+    <footer className="footer">
+      <div>
+        <img src={footerImage} />
+      </div>
+      <div>
+        <p>Copyright Little Lemon</p>
+      </div>
+    </footer>
   );
 };
 
