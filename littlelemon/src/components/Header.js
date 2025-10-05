bbf439723566074b6e1d5c16a24e131b14ae48d0
Oisin <oisin.leonard@gmail.com>
2025-10-05 09:46:15 +0100

    Wrapped navbar in header

--- littlelemon/src/components/Header.js
+++ littlelemon/src/components/Header.js
@@ -2,23 +2,25 @@ import logo from "../assets/Logo.svg";
 
 const Header = () => {
   return (
-    <nav className="nav">
-        <img src={logo} alt="logo" />
-      <ul className="nav-links">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/menu">Menu</a>
-        </li>
-        <li>
-          <a href="/bookings">Bookings</a>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <nav className="nav">
+          <img src={logo} alt="logo" />
+        <ul className="nav-links">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/menu">Menu</a>
+          </li>
+          <li>
+            <a href="/bookings">Bookings</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
