5f857716f7e4aa379c8ff73d9acfa36c4fef1d4e
Oisin <oisin.leonard@gmail.com>
2025-10-06 19:26:02 +0100

    Created booking confirmation page

---
+++ littlelemon/src/components/pages/BookingConfirmation.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ConfirmedBooking = () => {
+  return (
+    <>
+      <section className="confirm">
+        <article>
+          <h1>
+          Booking has been <span>Confirmed!</span>
+          </h1>
+        </article>
+      </section>
+    </>
+  );
+};
+
+export default ConfirmedBooking;
