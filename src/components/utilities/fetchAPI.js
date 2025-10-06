19bcd16a5710f81e1898e4b9437effe96cb42b7b
Oisin <oisin.leonard@gmail.com>
2025-10-06 19:59:53 +0100

    Moved fetchAPI functions to separate utilities directory

---
+++ src/components/utilities/fetchAPI.js
@@ -0,0 +1,18 @@
+import seededRandom from "./seededRandom";
+
+const fetchAPI = function(date) {
+    let result = [];
+    let random = seededRandom(date.getDate());
+
+    for(let i = 17; i <= 23; i++) {
+        if(random() < 0.5) {
+            result.push(i + ':00');
+        }
+        if(random() < 0.5) {
+            result.push(i + ':30');
+        }
+    }
+    return result;
+};
+
+export default fetchAPI;
