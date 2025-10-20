d018c7b63fabf0b4687999baca1247b0951cbc3a
Oisin <oisin.leonard@gmail.com>
2025-10-20 21:53:13 +0100

    Dropped extra /

--- src/components/utilities/littleLemonAPI.js
+++ src/components/utilities/littleLemonAPI.js
@@ -1,7 +1,7 @@
 /**
  * const { default: littleLemonAPI } = await import("./src/components/utilities/littleLemonAPI.js");
- * await littleLemonAPI(url="http://localhost:8000//api/menu-items", method="GET")
- * await littleLemonAPI(url="http://localhost:8000//api/menu-items/1", method="GET")
+ * await littleLemonAPI(url="http://localhost:8000/api/menu-items", method="GET")
+ * await littleLemonAPI(url="http://localhost:8000/api/menu-items/1", method="GET")
  */
 const littleLemonAPI = async function(url, method, body=null, headers={"Accept":"*/*"}) {
     const response = fetch(url, {method:method, body:body, headers:headers});
