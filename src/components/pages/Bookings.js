8fc0ec838fcddae80005d9039ea9ccacb1db7374
Oisin <oisin.leonard@gmail.com>
2025-12-04 22:06:07 +0000

    Added logic to extract form values into a javscript object

--- src/components/pages/Bookings.js
+++ src/components/pages/Bookings.js
@@ -6,6 +6,15 @@ const Bookings = (props) => {
   const navigate = useNavigate();
   function submitForm(formData) {
     if (props.submitAPI(formData)) {
+      /* add rest api call to write booking info to backend */
+      const data = {
+        fullName:formData.target[1].attributes.value.nodeValue,
+        mobileNumber:formData.target[2].attributes.value.nodeValue,
+        numberGuests:formData.target[3].attributes.value.nodeValue,
+        dateTime:formData.target[4].attributes.value.nodeValue,
+        comment:formData.target[5].attributes.value.nodeValue
+      };
+      console.log(data);
       navigate("/confirmed");
     }
   }
