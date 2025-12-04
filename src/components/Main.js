83bc31cef60013214308410cf2f38fbcb11003f8
Oisin <oisin.leonard@gmail.com>
2025-12-04 21:02:32 +0000

    Removed deprecated available times and dispatch function

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route   } from "react-router-dom";
-import { useReducer, useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 
-import fetchAPI  from "./utilities/fetchAPI";
 import littleLemonAPI  from "./utilities/littleLemonAPI";
 import Home from "./pages/Home";
 import About from "./pages/About"
@@ -37,13 +36,6 @@ const Main = () => {
       return true;
   };
 
-  function updateTimes(state, date) {
-    return fetchAPI(new Date(date));
-  }
-
-  const initialState = fetchAPI(new Date());
-  const [state, dispatch] = useReducer(updateTimes, initialState);
-
   return (
     <main>
       <Router>
@@ -52,7 +44,7 @@ const Main = () => {
           <Route path="/about" element={<About />} />
           <Route path="/menu" element={<Menu menuItems={menuItems} />} />
           <Route path="/menu/:name" element={<MenuItem menuItems={menuItems} />} />
-          <Route path="/book" element={<Bookings availableTimes={state} dispatch={dispatch} submitAPI={submitAPI} />} />
+          <Route path="/book" element={<Bookings submitAPI={submitAPI} />} />
           <Route path="/confirmed" element={<BookingConfirmation />} />
         </Routes>
       </Router>
