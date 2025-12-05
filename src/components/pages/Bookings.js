4523addb9dff4aaad4bf063d3afd0a407fee9cc0
Oisin <oisin.leonard@gmail.com>
2025-12-05 11:52:41 +0000

    Added littleLemonAPI call to write booking details to database

--- src/components/pages/Bookings.js
+++ src/components/pages/Bookings.js
@@ -1,20 +1,34 @@
 import {useNavigate} from "react-router-dom";
 import BookingForm from "./forms/BookingForm";
 import aboutImage from "../../assets/mario-and-adrian.jpg";
+import littleLemonAPI  from "../utilities/littleLemonAPI";
+import { useRef } from "react";
 
 const Bookings = (props) => {
+
+  /* determine url endpoint for rest api */
+  const userPlatform = navigator.platform;
+  let apiUrl;
+  if (userPlatform.indexOf("Win") !== -1) {
+    apiUrl = "http://localhost:8000/api/bookings/";
+  } else {
+    apiUrl = "http://host.docker.internal:8000/api/bookings/";
+  }
+  var url=useRef(apiUrl);
+
   const navigate = useNavigate();
-  function submitForm(formData) {
+  async function submitForm(formData) {
     if (props.submitAPI(formData)) {
       /* add rest api call to write booking info to backend */
       const data = {
-        fullName:formData.target[1].attributes.value.nodeValue,
-        mobileNumber:formData.target[2].attributes.value.nodeValue,
-        numberGuests:formData.target[3].attributes.value.nodeValue,
-        dateTime:formData.target[4].attributes.value.nodeValue,
+        full_name:formData.target[1].attributes.value.nodeValue,
+        mobile_number:formData.target[2].attributes.value.nodeValue,
+        guest_number:formData.target[3].attributes.value.nodeValue,
+        date_time:formData.target[4].attributes.value.nodeValue,
         comment:formData.target[5].attributes.value.nodeValue
       };
-      console.log(data);
+      await littleLemonAPI(url.current, "POST", JSON.stringify(data), {"Accept":"*/*", "Content-Type": "application/json"});
+      console.log(JSON.stringify(data));
       navigate("/confirmed");
     }
   }
